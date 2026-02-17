3455aeb08e185bf538ecc36d46552d25541b923e
Hicham <relaxingchannel65@gmail.com>
2026-02-17 15:07:03 +0000

    commit

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -102,8 +102,8 @@ export default function Gallery() {
     { id: 38, category: "residences", image: samSalon },
     { id: 39, category: "residences", image: studioSalon },
 
-    { id: 40, category: "all", image: marjane },
-    { id: 41, category: "all", image: marjane2 },
+    { id: 40, category: "residences", image: marjane },
+    { id: 41, category: "residences", image: marjane2 },
 
     { id: 19, category: "business", image: lab1 },
   ];
